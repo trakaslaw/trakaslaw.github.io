4d13ca8eee8aa42e51897060534ae2bb1d4b883c
trakaslaw <40127954+trakaslaw@users.noreply.github.com>
2018-07-22 10:54:19 -0500

    Login Javascript with FireBase

---
+++ js/fireBase.js
@@ -0,0 +1,14 @@
+var app_fireBase = {};
+(function(){
+  // Initialize Firebase
+  var config = {
+  apiKey: "AIzaSyAq_1IvTbn7tq26DEII-ISYelW8WR3Jqyw",
+  authDomain: "fir-uiauth-9e763.firebaseapp.com",
+  databaseURL: "https://fir-uiauth-9e763.firebaseio.com",
+  projectId: "fir-uiauth-9e763",
+  storageBucket: "fir-uiauth-9e763.appspot.com",
+  messagingSenderId: "120885569986"
+  };
+  firebase.initializeApp(config);
+  app_fireBase = fireBase;
+})()
